4cc24d7749770e2d0e67bba6a7dfbe112c930ff4
maryblanc <mariablan.racing@gmail.com>
2026-05-13 13:28:25 +0200

    Fetch meals from API and display them

--- script.js
+++ script.js
@@ -1,17 +1,46 @@
-const meals = [
-  "Spaghetti",
-  "Curry",
-  "Pierogi",
-  "Pizza",
-  "Risotto"
-];
+const API_URL = https:"//script.google.com/macros/s/AKfycbw3iqfAISyny6x3z93j3-yQlLwLgsKw1Lyv2gJIfRB6TA1MFHrsawRD-71Kf1nVOmub1g/exec";
 
-document.getElementById("randomBtn").addEventListener("click", () => {
+let meals = [];
 
-  const randomMeal =
-    meals[Math.floor(Math.random() * meals.length)];
+async function loadMeals() {
 
-  document.getElementById("mealContainer").innerHTML = `
-    <h2>${randomMeal}</h2>
-  `;
+  const response = await fetch(API_URL);
+
+  meals = await response.json();
+
+  displayMeals(meals);
+}
+
+function displayMeals(meals) {
+
+  const container =
+    document.getElementById("mealContainer");
+
+  container.innerHTML = "";
+
+  meals.forEach(meal => {
+
+    const div = document.createElement("div");
+
+    div.innerHTML = `
+      <h3>${meal.name}</h3>
+      <p>Cook time: ${meal.cook_time} min</p>
+      <p>Effort: ${meal.effort}</p>
+      <hr>
+    `;
+
+    container.appendChild(div);
+  });
+}
+
+document
+  .getElementById("randomBtn")
+  .addEventListener("click", () => {
+
+    const randomMeal =
+      meals[Math.floor(Math.random() * meals.length)];
+
+    alert(`Today's meal: ${randomMeal.name}`);
 });
+
+loadMeals();
